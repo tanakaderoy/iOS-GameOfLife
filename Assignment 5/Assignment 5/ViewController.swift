3fe1a10e133ccc2d8470df0fce432478bf1516aa
Student <student@Michaels-iMac.local>
2019-03-12 18:17:16 -0400

    Create Project

---
+++ Assignment 5/Assignment 5/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment 5
+//
+//  Created by Student on 3/12/19.
+//  Copyright © 2019 Tanaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
